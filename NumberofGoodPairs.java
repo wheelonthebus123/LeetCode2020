746581a53a5da2c3bf0bb20e7d2300fa5738047c
wheelonthebus123 <40865496+wheelonthebus123@users.noreply.github.com>
2020-07-12 00:20:01 -0700

    NumberofGoodPairs.java

---
+++ NumberofGoodPairs.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberofGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for(int v:nums)
+        {
+            counts.put(v, counts.get(v)+1);
+        }
+
+        int ret = 0;
+        for(Map.Entry e : counts.entrySet())
+        {
+            int n = (int) e.getValue();
+            if(n>1)
+            {
+                ret += n*(n-1)/2;
+            }
+        }
+
+        return ret;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
