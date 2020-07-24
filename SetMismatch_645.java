4c28b66df8401cf2da36181543e435b14c3bf2c6
wheelonthebus123 <40865496+wheelonthebus123@users.noreply.github.com>
2020-07-24 07:05:05 -0700

    SetMismatch_645.java

---
+++ SetMismatch_645.java
@@ -0,0 +1,20 @@
+public class SetMismatch_645 {
+    public int[] findErrorNums(int[] nums) {
+        int[] ret = new int[2];
+        for(int i : nums){
+            if(nums[Math.abs(i)-1]<0){
+                ret[0] = Math.abs(i);
+            }else{
+                nums[Math.abs(i)-1] *= -1;
+            }
+        }
+
+        for(int i=0; i<nums.length; ++i){
+            if(nums[i]>0){
+                ret[1] = Math.abs(i) + 1;
+            }
+        }
+
+        return ret;
+    }
+}
